16122a5fa5be4ca1286b5d07cae111afa350e2b7
shubham0406199 <74084472+shubham0406199@users.noreply.github.com>
2022-12-22 14:41:12 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,1 +1,14 @@
-
+module.exports = {
+  endpoint: 'https://github.com/api/v3',
+  hostRules:[
+  {
+      hostType:'github',
+      matchHost:'github.com'
+  }
+  ],
+  autodiscover:false,
+  repositories:[],
+  "pre-commit":{
+  }
+};
+  
